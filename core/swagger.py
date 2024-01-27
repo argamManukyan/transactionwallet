2386c1d41955d4523e3e3bf966ee8a9b60880969
argamManukyan <62460275+argamManukyan@users.noreply.github.com>
2024-01-27 15:36:09 +0400

    added swagger tagging

--- core/swagger.py
+++ core/swagger.py
@@ -1,24 +1,40 @@
 from django.urls import path
+from drf_yasg.inspectors import SwaggerAutoSchema
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Wallet API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny]
+    openapi.Info(
+        title="Wallet API",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
 )
 
 swagger_urlpatterns = [
-   # path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+
+class CustomAutoSchema(SwaggerAutoSchema):
+    """Add tags in Swagger auto schema"""
+
+    def get_tags(self, operation_keys=None):
+        return (
+            self.overrides.get("tags", None)
+            or getattr(self.view, "tags", [])
+            or [operation_keys[0]]
+        )
