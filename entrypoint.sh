ac53ed68408a120321845d74b651726b3d6777fa
argamManukyan <62460275+argamManukyan@users.noreply.github.com>
2024-01-27 15:37:03 +0400

    added tests in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+export $(grep -v '^#' .env | xargs)
 mkdir -p static
 python3 manage.py collectstatic --noinput
+
 python3 manage.py migrate
 echo "Migrations are passed"
+
+echo "Running tests"
+
+python3 manage.py test
 
 python3 manage.py runserver 0.0.0.0:8000
 echo "Server is running"
